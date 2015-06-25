67fa45da8da4a74abaef50ab2fa6b60ce3dc95c6
Alan Rempel <alan.rempel@gmail.com>
2015-06-24 23:21:11 -0500

    Macros can now be replayed.
    The replay function takes a Macro and an Address and returns a Task
    which sends each of the actions recorded in the macro to the
    address in the order in which they were recorded. There might be a
    bug that makes this function swallow absurd amounts of memory ---
    or the bug might be here but somewhere else. I don't know yet.
    
    I also made some of the code more readable and prettier.

--- source/Macro.elm
+++ source/Macro.elm
@@ -1,12 +1,19 @@
 module Macro where
 
-import Signal exposing ( Signal, map, map2, foldp )
+import Signal exposing ( Signal, Address, map, map2, foldp, send )
 import Signal.Extra exposing ( keepThen, keepWhen, sampleWhen, filter )
+
+import Task exposing ( Task, andThen )
+
+import List
 
 import Maybe exposing ( Maybe(..) )
 import Maybe.Extra exposing ( isJust, isNothing )
 
 
+-- A macro is just a list of actions, whatever your action type may be.
+-- The functions in this module all build up macros from right to left, 
+-- which means right is earlier and left is later.
 type alias Macro action = List action
 
 record :  Signal Bool           -- whether or not we're recording;
@@ -15,7 +22,8 @@ record :  Signal Bool           -- whether or not we're recording;
 
 record recording actions =
 
-        let macroActions =
+        let 
+            macroActions =
                     keepThen recording Nothing
                             <| map Just actions
 
@@ -26,7 +34,8 @@ record recording actions =
 
             macroSignal = foldp prependMaybe [] macroActions
 
-         in keepWhen (map not recording) [] macroSignal
+         in 
+            keepWhen (map not recording) [] macroSignal
 
 
 type alias NamedMacro action =
@@ -40,7 +49,28 @@ recordNamed :  Signal (Maybe String)      -- whether or not we're recording,
             -> Signal (NamedMacro action) -- the named macro.
 
 recordNamed mNames actions =
-        let recording = map isJust mNames
+        let 
+            recording = map isJust mNames
             macros = record recording actions
-            namedMacro macro name = { macro = macro, name = name }
-         in map2 namedMacro macros <| filter "" mNames
+            namedMacro macro name =
+                        { macro = macro, name = name }
+         in 
+            map2 namedMacro macros <| filter "" mNames
+
+
+replay :  Address action -- the address to which to send the actions;
+       -> Macro action   -- the macro whose actions will be replayed in order;
+       -> Task x ()      -- a Task that executes the replay.
+
+replay address macro = 
+            let append action task =
+                            task `and` send address action
+             in List.foldr append
+                           ( Task.succeed () )
+                           macro
+
+-- a utility function that simply sequences two Tasks,
+-- where the second Task does not depend on the return
+-- value of the first.
+and : Task x () -> Task x a -> Task x a
+and task1 task2 = task1 `andThen` \ () -> task2

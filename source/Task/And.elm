ffd4faafc9a017aa2b5b6dda0ed075bd0a89f717
Alan Rempel <alan.rempel@gmail.com>
2015-09-02 18:13:42 -0500

    Generalized Task.And.and.
    Just for the sake of potential convenience. Gratuitous
    generalization? But why force someone to write `Task.map (always ())`?

--- source/Task/And.elm
+++ source/Task/And.elm
@@ -9,5 +9,5 @@ import Task exposing ( Task, andThen )
 {-| a utility function that simply sequences two Tasks,
 where the second Task does not depend on the first.
 -}
-and : Task x () -> Task x a -> Task x a
-and task1 task2 = task1 `andThen` \ () -> task2
+and : Task x b -> Task x a -> Task x a
+and task1 task2 = task1 `andThen` \ _ -> task2

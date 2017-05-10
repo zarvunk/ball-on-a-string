af739d68da54bc3c2d43af6cdc6492a3e6307fc4
Alan Rempel <alan.rempel@gmail.com>
2017-05-10 12:54:20 -0300

    Format all source files with elm-format

--- source/App.elm
+++ source/App.elm
@@ -1,23 +1,20 @@
-module App exposing ( main )
+module App exposing (main)
 
-import Html exposing ( Html, program, body )
-import Svg exposing ( Svg, svg, circle )
-import Svg.Attributes as Attrs exposing ( cx, cy, r, fill )
-
-import Time exposing ( Time )
+import Html exposing (Html, program, body)
+import Svg exposing (Svg, svg, circle)
+import Svg.Attributes as Attrs exposing (cx, cy, r, fill)
+import Time exposing (Time)
 import Color
 import Char
-
 import Keyboard
 import AnimationFrame as Frame
-
-import Draggable as Drag exposing ( Delta )
+import Draggable as Drag exposing (Delta)
 import Mechanics
-import CssBasics as Css exposing ( CssValue, encodeCssValue )
-import CssBasics.Properties exposing ( property )
+import CssBasics as Css exposing (CssValue, encodeCssValue)
+import CssBasics.Properties exposing (property)
 import Stylesheet exposing (..)
-
 import Ball exposing (..)
+
 
 main : Program Never State Msg
 main =
@@ -25,7 +22,9 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = subscriptions }
+        , subscriptions = subscriptions
+        }
+
 
 type alias State =
     { ball : Ball
@@ -35,126 +34,169 @@ type alias State =
     , paused : Bool
     }
 
-type Msg = DragMoveBy Delta
-         | DragMsg (Drag.Msg EntityID)
-         | NextFrame Time
-         | SetPaused Bool
-         | DoNothing
 
-init : (State, Cmd Msg)
-init = ( { ball = initialBall
-         , elastic = initialElastic
-         , friction = initialFriction
-         , drag = Drag.init
-         , paused = False
-         }
-       , Cmd.none )
+type Msg
+    = DragMoveBy Delta
+    | DragMsg (Drag.Msg EntityID)
+    | NextFrame Time
+    | SetPaused Bool
+    | DoNothing
 
-update : Msg -> State -> (State, Cmd Msg)
+
+init : ( State, Cmd Msg )
+init =
+    ( { ball = initialBall
+      , elastic = initialElastic
+      , friction = initialFriction
+      , drag = Drag.init
+      , paused = False
+      }
+    , Cmd.none
+    )
+
+
+update : Msg -> State -> ( State, Cmd Msg )
 update msg ({ ball, elastic, friction } as state) =
     case msg of
         DragMoveBy delta_xy ->
-            ( { state | elastic =
-                { elastic | being = move delta_xy elastic.being } }
-            , Cmd.none )
+            ( { state
+                | elastic =
+                    { elastic | being = move delta_xy elastic.being }
+              }
+            , Cmd.none
+            )
+
         DragMsg dragMsg ->
             Drag.update dragConfig dragMsg state
+
         NextFrame delta_t ->
             let
-                fields = [ accelOf friction, accelOf elastic ]
+                fields =
+                    [ accelOf friction, accelOf elastic ]
             in
-                ( { state | ball =
+                ( { state
+                    | ball =
                         ball |> actOn fields delta_t
-                  }, Cmd.none )
+                  }
+                , Cmd.none
+                )
+
         SetPaused areWePaused ->
             ( { state | paused = areWePaused }
-            , Cmd.none )
+            , Cmd.none
+            )
+
         DoNothing ->
             ( state
-            , Cmd.none )
+            , Cmd.none
+            )
+
 
 subscriptions : State -> Sub Msg
 subscriptions { drag, paused } =
     Sub.batch <|
-        if paused
-        then
+        if paused then
             [ Keyboard.presses handleKeyPressPaused ]
         else
             [ Keyboard.presses handleKeyPressPlaying
             , Frame.diffs NextFrame
-            , Drag.subscriptions DragMsg drag ]
+            , Drag.subscriptions DragMsg drag
+            ]
+
 
 view : State -> Html Msg
 view { ball, elastic } =
     let
         styleRules =
-          newRuleSet
-            |> withSelectors
-              [ Tag "html", Tag "body" ]
-            |> withDeclarations
-              [ property.width := Css.Str "100%"
-              , property.height := Css.Str "100%"
-              , property.display := Css.Str "block" ]
+            newRuleSet
+                |> withSelectors
+                    [ Tag "html", Tag "body" ]
+                |> withDeclarations
+                    [ property.width := Css.Str "100%"
+                    , property.height := Css.Str "100%"
+                    , property.display := Css.Str "block"
+                    ]
+
         stylesheet =
-            toStyleNode <| withRules [styleRules] newStylesheet
+            toStyleNode <| withRules [ styleRules ] newStylesheet
+
         svgBall =
-            circle [
-              cx <| toString (x ball)
-            , cy <| toString (y ball)
-            , r  <| toString <| ball.radius
-            , fill <| encodeCssValue (Css.Col ball.color)
-            , Drag.mouseTrigger elastic.being.id DragMsg
-            ]
-            []
-     in
+            circle
+                [ cx <| toString (x ball)
+                , cy <| toString (y ball)
+                , r <| toString <| ball.radius
+                , fill <| encodeCssValue (Css.Col ball.color)
+                , Drag.mouseTrigger elastic.being.id DragMsg
+                ]
+                []
+    in
         body
-        []
-        [ stylesheet
-        , svg [
-            Attrs.width "100%"
-          , Attrs.height "100%"
-          ]
-          [ svgBall ]
-        ]
+            []
+            [ stylesheet
+            , svg
+                [ Attrs.width "100%"
+                , Attrs.height "100%"
+                ]
+                [ svgBall ]
+            ]
+
 
 (:=) : String -> CssValue -> Css.Declaration
-(:=) = (,)
+(:=) =
+    (,)
+
 
 dragConfig : Drag.Config EntityID Msg
 dragConfig =
     Drag.basicConfig DragMoveBy
 
+
 initialBall : Ball
 initialBall =
-    { state = Mechanics.state2 (0, 0) (0, 0)
+    { state = Mechanics.state2 ( 0, 0 ) ( 0, 0 )
     , id = 1
     , color = Color.green
     , radius = 24
     }
 
+
 initialElastic : HasField Entity
 initialElastic =
     let
-        strength = 0.00008
+        strength =
+            0.00008
+
         being =
-            { state = Mechanics.state2 (200, 0) (200, 0)
-            , id = 2 }
+            { state = Mechanics.state2 ( 200, 0 ) ( 200, 0 )
+            , id = 2
+            }
     in
         elastic strength being
+
 
 initialFriction : HasField ()
 initialFriction =
     friction 0.001
 
+
 handleKeyPressPaused : Char.KeyCode -> Msg
 handleKeyPressPaused code =
     case Char.fromCode code of
-        ' ' -> NextFrame 0.1
-        'p' -> SetPaused False
-        _   -> DoNothing
+        ' ' ->
+            NextFrame 0.1
+
+        'p' ->
+            SetPaused False
+
+        _ ->
+            DoNothing
+
 
 handleKeyPressPlaying : Char.KeyCode -> Msg
 handleKeyPressPlaying code =
     case Char.fromCode code of
-        'p' -> SetPaused True
-        _   -> DoNothing
+        'p' ->
+            SetPaused True
+
+        _ ->
+            DoNothing
--- source/Ball.elm
+++ source/Ball.elm
@@ -1,136 +1,187 @@
 module Ball exposing (..)
 
-
-import Color exposing ( Color )
-import Time exposing ( Time )
-
+import Color exposing (Color)
+import Time exposing (Time)
 import Function
 import Tuple2 exposing (..)
-
-import Draggable as Drag exposing ( Delta )
+import Draggable as Drag exposing (Delta)
 import Mechanics
 
+
 type alias Is e =
-    { e | state : Mechanics.State
+    { e
+        | state : Mechanics.State
         , id : EntityID
     }
 
-type alias Entity = Is {}
 
-type alias EntityID = Int
+type alias Entity =
+    Is {}
+
+
+type alias EntityID =
+    Int
+
 
 type alias Circular c =
-              { c | color : Color
-                  , radius : Float }
+    { c
+        | color : Color
+        , radius : Float
+    }
 
-type alias Ball = Circular Entity
+
+type alias Ball =
+    Circular Entity
+
 
 type alias HasField locus =
-    { field : locus
-              -- the state of the locus of the field
-           -> Mechanics.State
-              -- the state of the thing affected by the
-              -- field
-           -> List Float
-              -- the changes to the thing's velocities
-    , being : locus }
+    { field :
+        locus
+        -- the state of the locus of the field
+        -> Mechanics.State
+        -- the state of the thing affected by the
+        -- field
+        -> List Float
 
-elastic : Float -- the strength of the elastic
-       -> Is thing
-       -> HasField (Is thing)
+    -- the changes to the thing's velocities
+    , being : locus
+    }
+
+
+elastic :
+    Float -- the strength of the elastic
+    -> Is thing
+    -> HasField (Is thing)
 elastic strength being =
     let
         field locus body =
             let
-                distance_x = x locus - x_ body
-                distance_y = y locus - y_ body
-                accel_x = distance_x * strength
-                accel_y = distance_y * strength
+                distance_x =
+                    x locus - x_ body
+
+                distance_y =
+                    y locus - y_ body
+
+                accel_x =
+                    distance_x * strength
+
+                accel_y =
+                    distance_y * strength
             in
                 [ accel_x, accel_y ]
-     in
+    in
         { being = being
-        , field = field }
+        , field = field
+        }
 
-friction : Float -- the coefficient of drag
-        -> HasField ()
+
+friction :
+    Float -- the coefficient of drag
+    -> HasField ()
 friction drag =
     let
         field _ body =
             let
-                velocity_x = vx_ body
-                velocity_y = vy_ body
-                decel_x = decelerate drag velocity_x
-                decel_y = decelerate drag velocity_y
+                velocity_x =
+                    vx_ body
+
+                velocity_y =
+                    vy_ body
+
+                decel_x =
+                    decelerate drag velocity_x
+
+                decel_y =
+                    decelerate drag velocity_y
             in
                 [ decel_x, decel_y ]
-     in
+    in
         { being = ()
-        , field = field }
+        , field = field
+        }
+
 
 decelerate : Float -> Float -> Float
 decelerate drag velocity =
     let
-        acceleration = min drag (abs velocity)
-     in
-        if velocity >= 0
-           then -acceleration
-           else  acceleration
+        acceleration =
+            min drag (abs velocity)
+    in
+        if velocity >= 0 then
+            -acceleration
+        else
+            acceleration
+
 
 move : Delta -> Is thing -> Is thing
-move (dx, dy) thing =
-    { thing | state = Mechanics.state2
-                        -- we assume two-dimensional coordinates
-                        (x thing + dx, vx thing)
-                        (y thing + dy, vy thing)
+move ( dx, dy ) thing =
+    { thing
+        | state =
+            Mechanics.state2
+                -- we assume two-dimensional coordinates
+                ( x thing + dx, vx thing )
+                ( y thing + dy, vy thing )
     }
+
 
 x : Is thing -> Float
 x =
     x_ << .state
 
+
 y : Is thing -> Float
 y =
     y_ << .state
+
 
 vx : Is thing -> Float
 vx =
     vx_ << .state
 
+
 vy : Is thing -> Float
 vy =
     vy_ << .state
+
 
 x_ : Mechanics.State -> Float
 x_ =
     Mechanics.coordinate 0
 
+
 y_ : Mechanics.State -> Float
 y_ =
     Mechanics.coordinate 1
+
 
 vx_ : Mechanics.State -> Float
 vx_ =
     Mechanics.velocity 0
 
+
 vy_ : Mechanics.State -> Float
 vy_ =
     Mechanics.velocity 1
 
+
 accelOf : HasField being -> Mechanics.State -> List Float
 accelOf { being, field } =
     field being
+
 
 actOn : List (Mechanics.State -> List Float) -> Time -> Is thing -> Is thing
 actOn accels dt ({ state } as body) =
     let
         addAccels =
             Function.map2 (List.map2 (+))
+
         zeroAccel =
             -- we're working in 2D only
-            always [0, 0]
+            always [ 0, 0 ]
+
         totalAccel =
-            accels |> List.foldr addAccels zeroAccel
-                   |> Mechanics.acceleration
+            accels
+                |> List.foldr addAccels zeroAccel
+                |> Mechanics.acceleration
     in
         { body | state = Mechanics.evolve totalAccel dt state }
